9ea63208035dbca7e9fdd809d8d12f656171f4f2
Jakov <sikiric.jakov@outlook.com>
2023-10-22 18:27:03 +0200

    server log

--- server/src/09-webapi.ts
+++ server/src/09-webapi.ts
@@ -44,11 +44,13 @@ const httpsServer = https.createServer(httpsOptions, app);
 const port = 3000;
 const host = "0.0.0.0";
 
-const server = app.listen(3000, "0.0.0.0", () => {
+const server = app.listen(3000, () => {
   const serverAddress = server.address();
   if (typeof serverAddress === "string") {
     console.error("Server address is a string, not available.");
   } else {
-    console.log(`Express server started on host ${serverAddress}`);
+    console.log(
+      `Express server started on host ${serverAddress?.address} and port ${serverAddress?.port}`
+    );
   }
 });
