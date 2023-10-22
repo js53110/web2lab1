a0bb39c19099da73a722471b44b0bf5f2673a7de
Jakov <sikiric.jakov@outlook.com>
2023-10-22 18:34:00 +0200

    443

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const serverHostname = "0.0.0.0"; // Replace with your Render app's domain
-const serverPort = 3000; // Use 443 for HTTPS
+const serverPort = 443; // Use 443 for HTTPS
 
-const serverUri = `https://${serverHostname}`; //:${serverPort}`;
+const serverUri = `https://${serverHostname}:${serverPort}`;
 
 const request = (accessToken) => {
   console.log("sending request to: " + serverUri);
