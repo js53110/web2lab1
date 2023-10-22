f1b0df59d8be0481309f8a2480f596e71401d23c
Jakov <sikiric.jakov@outlook.com>
2023-10-22 18:08:50 +0200

    a

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const serverHostname = "web2lab1-888d.onrender.com"; // Replace with your Render app's domain
 const serverPort = 3000; // Use 443 for HTTPS
 
-const serverUri = `https://${serverHostname}:${serverPort}`;
+const serverUri = `http://${serverHostname}:${serverPort}`;
 
 const request = (accessToken) => {
   console.log("sending request to: " + serverUri);
--- server/src/09-webapi.ts
+++ server/src/09-webapi.ts
@@ -44,6 +44,11 @@ const httpsServer = https.createServer(httpsOptions, app);
 const port = 3000;
 const host = "0.0.0.0";
 
-app.listen(port, host, () => {
-  console.log(`Web API running at port ${port}/`);
+const server = app.listen(3000, "0.0.0.0", () => {
+  const serverAddress = server.address();
+  if (typeof serverAddress === "string") {
+    console.error("Server address is a string, not available.");
+  } else {
+    console.log(`Express server started on host ${serverAddress}`);
+  }
 });
