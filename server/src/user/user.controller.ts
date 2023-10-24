95942f5be459a49da8936963c1c20a12ded119e1
Jakov <sikiric.jakov@outlook.com>
2023-10-24 14:08:49 +0200

    controller

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 import { User } from "../shared/database/index";
 import { CREATED } from "http-status";
+import { auth } from "express-oauth2-jwt-bearer";
 
 const login = async (req: any, res: any) => {
   console.log("IN LOGIN API SERVER SIDE");
   const authServer = "https://js53110.eu.auth0.com";
+
+  const jwtCheck = auth({
+    audience: "https://js53110.com", // Audience should match your Auth0 API
+    issuerBaseURL: "https://js53110.eu.auth0.com",
+    tokenSigningAlg: "RS256",
+  });
+
   const accessToken = req.auth!.token;
   try {
     const userInfoResponse = await axios.post(
